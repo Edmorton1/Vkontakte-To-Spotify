afbd97bf5ccdae65b82ff5b89cde348442c49ecb
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-04-04 13:32:38 +0500

    Убрал логи в прод, забыл билд

--- src/URLS.ts
+++ src/URLS.ts
@@ -1,9 +1,9 @@
-export const URL_SERVER = `http://localhost:3000/`
-export const URL_SERVER_API = `${URL_SERVER}api/`
-export const URL_SERVER_WS = `ws://localhost:3000/`
-export const URL_CLIENT = `http://localhost:5000/`
-
-// export const URL_SERVER = `https://server-otaku-vibes.up.railway.app/`
+// export const URL_SERVER = `http://localhost:3000/`
 // export const URL_SERVER_API = `${URL_SERVER}api/`
-// export const URL_SERVER_WS = `wss://server-otaku-vibes.up.railway.app/`
-// export const URL_CLIENT = `https://client-otaku-vibes.up.railway.app/`
+// export const URL_SERVER_WS = `ws://localhost:3000/`
+// export const URL_CLIENT = `http://localhost:5000/`
+
+export const URL_SERVER = `https://server-otaku-vibes.up.railway.app/`
+export const URL_SERVER_API = `${URL_SERVER}api/`
+export const URL_SERVER_WS = `wss://server-otaku-vibes.up.railway.app/`
+export const URL_CLIENT = `https://client-otaku-vibes.up.railway.app/`
